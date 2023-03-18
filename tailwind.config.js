86c554910e81ad9d054bd5edbe91ce501db7613d
Gurubalan Harikrishnan <gurubalan.work@gmail.com>
2023-03-18 11:29:37 +0530

    build: adding package dependencies and architecture

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const { fontFamily } = defaultTheme;
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./common/components/**/*.{js,ts,jsx,tsx}",
+    "./common/components-ui/**/*.{js,ts,jsx,tsx}",
+    "./common/components-helpers/**/*.{js,ts,jsx,tsx}",
+    "./modules/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./common/constants/**/*.{js,ts}",
+  ],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1200px",
+      xxl: "1440px",
+    },
+    extend: {},
+    fontFamily: {
+      sans: ["Inter", ...fontFamily.sans],
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+};
