656bbf8bac37e734f227a9f3be44db9a4ce48c13
Gurubalan Harikrishnan <gurubalan.work@gmail.com>
2023-03-30 22:29:47 +0530

    bug fix: fixes for filter bugs, code improvements

--- common/utils/helpers/filters.ts
+++ common/utils/helpers/filters.ts
@@ -2,7 +2,6 @@ import { isObjectEmpty } from "@common/utils/helpers/global";
 import { filterActions } from "./hashmaps";
 export const compoundFilters = {};
 
-// #TESTME : Possible bugs when deletions are traversed from outer depth levels, need to clear parent objects when inner child is deleted from outer depths
 export const performFilterAction = (
   rootFilters: any,
   depth: number,
@@ -84,39 +83,53 @@ export const getNestedUpdate = (
   }
 
   if (rootFilters?.and) {
-    return {
-      ...rootFilters,
-      and: rootFilters.and
-        ?.map((rootFilter: any) =>
-          getNestedUpdate(
-            rootFilter,
-            depth - 1,
-            pathIndex,
-            nestedFilter,
-            action,
-            compoundFilterKey
-          )
+    const updatedFilters = rootFilters.and
+      ?.map((rootFilter: any) =>
+        getNestedUpdate(
+          rootFilter,
+          depth - 1,
+          pathIndex,
+          nestedFilter,
+          action,
+          compoundFilterKey
         )
-        .filter((rootFilter: any) => rootFilter && !isObjectEmpty(rootFilter)),
-    };
+      )
+      .filter((rootFilter: any) => rootFilter && !isObjectEmpty(rootFilter));
+
+    if (!!updatedFilters.length) {
+      return {
+        ...rootFilters,
+        and: updatedFilters,
+      };
+    } else {
+      delete rootFilters.and;
+      return rootFilters;
+    }
   }
 
   if (rootFilters?.or) {
-    return {
-      ...rootFilters,
-      or: rootFilters.or
-        ?.map((rootFilter: any) =>
-          getNestedUpdate(
-            rootFilter,
-            depth - 1,
-            pathIndex,
-            nestedFilter,
-            action,
-            compoundFilterKey
-          )
+    const updatedFilters = rootFilters.or
+      ?.map((rootFilter: any) =>
+        getNestedUpdate(
+          rootFilter,
+          depth - 1,
+          pathIndex,
+          nestedFilter,
+          action,
+          compoundFilterKey
         )
-        .filter((rootFilter: any) => rootFilter && !isObjectEmpty(rootFilter)),
-    };
+      )
+      .filter((rootFilter: any) => rootFilter && !isObjectEmpty(rootFilter));
+
+    if (!!updatedFilters.length) {
+      return {
+        ...rootFilters,
+        or: updatedFilters,
+      };
+    } else {
+      delete rootFilters.or;
+      return rootFilters;
+    }
   }
 
   return rootFilters;
